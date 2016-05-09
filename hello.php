b02d32dcbd83e09c36c78498723e39c1f165dbcb
anugrahab <anugrahab@gmail.com>
2016-05-09 16:53:26 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,8 +1,7 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Message>Hello, Mobile Monkey</Message>
+</Response>
